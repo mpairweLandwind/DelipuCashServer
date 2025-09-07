bd4ce4e627dfaffe329fd95c1f20f8b45096caac
mpairweLandwind <mpairwelauben22@gmail.com>
2025-09-07 22:00:50 +0300

    updated index.js

--- index.js
+++ index.js
@@ -78,6 +78,11 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+if (process.env.VERCEL !== '1') {
+  const PORT = process.env.PORT || 8000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+export default app;
